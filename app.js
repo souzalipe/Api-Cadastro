f9d2e987aabcb7a7d02db54218b5da0815b7855d
Felipe Souza <fn23886@gmail.com>
2024-07-24 09:24:25 -0300

    app.js

--- app.js
+++ app.js
@@ -1,2 +1,14 @@
 import express from 'express';
+import { userRouter } from './src/router/user.router.js';
 
+const app = express();
+
+app.use(express.json()); // -> middlewear de conversão para json 
+
+app.use(userRouter);
+
+const Port = 3000;
+
+app.listen(Port, () => {
+    console.log(`Aplication ready on port  http://localhost:${Port}`);
+});
