d9a7e4cf201a7eeae5aa8fe1fab6331fb08f33c5
Felipe Souza <fn23886@gmail.com>
2024-08-05 17:33:31 -0300

    comment app.js

--- app.js
+++ app.js
@@ -12,29 +12,29 @@ app.use(userRouter);
 
 const Port = process.env.Port || 3000;
 
-// Nodemailer 
+// <<< Nodemailer >>> 
 
-const transport = nodemailer.createTransport({
-	host:'smtp.gmail.com', 
-	port: 465,
-	secure: true,
-	auth: {
-		user: 'fn23886@gmail.com', // meu email aqui 
-		pass: 'ovyc itfi gqdu bkzx' //senha do seu email gerado pelo google
-	}
-});
+// const transport = nodemailer.createTransport({
+// 	host:'smtp.gmail.com', 
+// 	port: 465,
+// 	secure: true,
+// 	auth: {
+// 		user: 'fn23886@gmail.com', // meu email aqui 
+// 		pass: 'ovyc itfi gqdu bkzx' //senha do seu email gerado pelo google
+// 	}
+// });
 
-transport.sendMail({
-	from: 'Gotas do Amanhã <fn23886@gmail.com>', //deve passar o mesmo email do remetente 
-	to: 'rrobotinho@gmail.com>',
-	subject: 'Seja Bem vindo ao Gotas do Amanhã',
-	html: '<h1>Olá, Seja bem vindo ao Gotas do Amanhã Felipe</h1>',
-	text: 'Estou Gotas do Amanhã',
-})
-.then(()=> console.log("Email enviado boy"))
-.catch((err)=>console.log('Erro ao enviar email: ', err));
+// transport.sendMail({
+// 	from: 'teste <fn23886@gmail.com>', //deve passar o mesmo email do remetente 
+// 	to: 'rrobotinho@gmail.com>',
+// 	subject: 'Seja Bem vindo ao Gotas do Amanhã',
+// 	html: '<h1>Olá, Seja bem vindo ao Gotas do Amanhã Felipe</h1>',
+// 	text: 'Estou Gotas do Amanhã',
+// })
+// .then(()=> console.log("Email enviado boy"))
+// .catch((err)=>console.log('Erro ao enviar email: ', err));
 
-// Sincronia de Dados
+// <<< Sincronia de Dados >>>
  
 sequelize
 	.sync()
